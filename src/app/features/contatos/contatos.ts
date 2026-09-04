91d4faeafc040a061bc3863fbac407a31e87c4ff
arthur da costa costa <arthurcosta2727@gmail.com>
2026-09-04 20:03:36 -0300

    feat: estrutura de pastas, componentes e rotas da etapa 4

---
+++ src/app/features/contatos/contatos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-contatos',
+  styleUrl: './contatos.css',
+  templateUrl: './contatos.html',
+})
+export class Contatos {}
